649da8b609f08d303e1244e3c8ad62f52fdb468c
KhadeejaShirinVP <khadeejasvp@moback.com>
2026-04-07 13:33:58 +0530

    Added global error handling

---
+++ Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Text.Json;
+using EmployeeApi.Common;
+namespace EmployeeApi.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleException((context,,ex));
+            }
+        }
+
+        private static Task HandleException(HttpContext context, Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new ApiResponse<string>
+            {
+                Success = false,
+                Message = ex.Message,
+                Data = null
+            };
+
+            var result = JsonSerializer.Serialize(response);
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
